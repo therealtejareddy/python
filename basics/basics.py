a2a8ac29460c01d368404bffcbaf7cdadb906280
Yuvateja <yuvatejareddy003@gmail.com>
2021-08-23 17:21:26 +0530

    python Basics 🐍

---
+++ basics/basics.py
@@ -0,0 +1,19 @@
+# Variables
+i1 = 10 # int
+i2 = 20
+s1 = "TEJA" # string
+f1 = 8.1 #float
+f2 = 7.33
+print(i1 + i2) # addition operator 
+print(i1 - i2) # sub operator
+print(i1 * i2) # multiplication operator
+print(i1 / i2) # division operator
+print(i1 // i2) # floor division operator
+print(i1 % i2) # modulo division operator
+print(i1 ** i2) # Exponential operator 
+
+# casting
+f3 = float(i1)
+i3 = int(f2)
+l1 = list(s1) # list
+t1 = tuple(s1) # tuple 
