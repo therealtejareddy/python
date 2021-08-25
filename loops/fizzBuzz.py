9936e8b1d0bd11dfdd5b22a42ee3459e912e3903
Yuvateja <yuvatejareddy003@gmail.com>
2021-08-25 15:08:16 +0530

    .

--- loops/fizzBuzz.py
+++ loops/fizzBuzz.py
@@ -5,14 +5,14 @@ and “FizzBuzz” if an integer is divisible by both 3 and 5
 """
 
 num = int(input("Enter Your Number: "))
-for i in range(num+1):
+for i in range(1,num+1):
     if i%3==0 and i%5==0:
         print("Fizzbuzz")
         continue
-    if i%3==0:
+    elif i%3==0:
         print("Fizz")
         continue
-    if i%5==0:
+    elif i%5==0:
         print("Buzz")
         continue
     else:
